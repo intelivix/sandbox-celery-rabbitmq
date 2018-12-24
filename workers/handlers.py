a5c1275bbeb80f06ae73b68a4304649a9b15efe2
Arthur Alvim <afmalvim@gmail.com>
2018-12-23 23:52:53 -0300

    Adicionando Dockerfile e docker-compose.yml. Integraçãom com o ECR.

--- workers/handlers.py
+++ workers/handlers.py
@@ -1,10 +1,14 @@
 from raven import Client
 from loafer.ext.sentry import sentry_handler
-from utils import sum_
 from decouple import config
 
 
+def sum_(x, y):
+    return x + y
+
+
 async def handler_add(data, *args):
+    print(data)
     return sum_(data['x'], data['y'])
 
 client = Client(config('SENTRY_TOKEN_URL'))
